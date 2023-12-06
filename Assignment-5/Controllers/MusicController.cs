ba9d9d55e1d6d595a1ad9e5967b8683b62c22938
nnickman <125490876+nnickman@users.noreply.github.com>
2023-12-05 21:22:54 -0600

    pushing main project

---
+++ Assignment-5/Controllers/MusicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_5.Controllers
+{
+    public class MusicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
