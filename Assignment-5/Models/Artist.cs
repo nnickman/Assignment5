f6b77241217ccabc5b48b4c37531b7bda76cb9d0
nnickman <125490876+nnickman@users.noreply.github.com>
2023-12-06 15:45:12 -0600

    committing db

--- Assignment-5/Models/Artist.cs
+++ Assignment-5/Models/Artist.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public decimal Price { get; set; }
 
         public int GenreID { get; set; }
     }
--- Assignment-5/Program.cs
+++ Assignment-5/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Assignment_5.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Assignment_5Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assignment_5Context") ?? throw new InvalidOperationException("Connection string 'Assignment_5Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
