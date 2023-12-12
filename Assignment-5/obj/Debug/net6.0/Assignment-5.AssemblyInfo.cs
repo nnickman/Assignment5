f7ed214dcd186ea4960b844edb87c3dd7d8dda64
Adam Job <adam.job@ndus.edu>
2023-12-11 21:04:43 -0600

    Updated View

--- Assignment-5/obj/Debug/net6.0/Assignment-5.AssemblyInfo.cs
+++ Assignment-5/obj/Debug/net6.0/Assignment-5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment-5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08df20078e3039b2c01d04ec404232815d4e8c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment-5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment-5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
