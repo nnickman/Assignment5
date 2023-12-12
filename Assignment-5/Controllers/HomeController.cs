9a8931d39f4f13860f5138a2298052a87f172308
nnickman <125490876+nnickman@users.noreply.github.com>
2023-12-11 19:12:36 -0600

    syncing

--- Assignment-5/Controllers/HomeController.cs
+++ Assignment-5/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Assignment_5.Models;
+﻿using Assignment_5.Data;
+using Assignment_5.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,11 +8,50 @@ namespace Assignment_5.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Assignment_5Context _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _context = _context;
         }
+
+        public async Task<IActionResult> Index(string genre, string artist)
+        {
+            if(_context == null)
+            {
+                return Problem("Assignment5Context.Songs is null");
+            }
+            var allSongs = from x in _context.Song
+                               select x;
+
+            var songs = from x in _context.Song
+                            select x;
+
+                string thisArtist = (from x in _context.Artist
+                                     where x.Name == artist
+                                     select x.Id).ToString();
+
+            
+            int thisArtistId;
+            Int32.TryParse(thisArtist, out thisArtistId);
+            
+
+            List<string> artistList = new List<string>();
+
+            if(!String.IsNullOrEmpty(genre)) 
+            {
+                songs = songs.Where(s => s.ArtistID == thisArtistId);
+            }
+            }
+        }
+
+
+
+
+
+
+
 
         public IActionResult Index()
         {
